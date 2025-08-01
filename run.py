5ccfb826f49a1ebd91dbd00da324d478396dea37
Priyanshu-Priyam <@Priyanshu2008>
2025-08-01 13:06:22 +0530

    added logging

--- run.py
+++ run.py
@@ -12,4 +12,4 @@ app = create_app()  # Gunicorn / Hypercorn will look for `app` at module level
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8080))
     # `debug=True` only when FLASK_ENV=development
-    app.run(host="0.0.0.0", port=port, debug=app.config.get("DEBUG", False))
+    app.run(host="0.0.0.0", port=port, debug=app.config.get("DEBUG", True))
